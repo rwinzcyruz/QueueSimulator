9466ca80266b7b4e43803135ec29eb729a600d6b
Erwin <rw7nz.cyruz@gmail.com>
2014-06-20 16:10:46 +0700

    add server idle time.

--- js/Server.js
+++ js/Server.js
@@ -1,36 +1,51 @@
 var Server = function () {
   var _id = ++Server.uuid,
       _totalServe = 0,
+      _queue = [],
+      _totalIdle = 0,
+      _idleTime = 0,
       _client,
-      _queue = [];
+      _isIdle = function () {
+        return !_client;
+      },
+      _getAvgIdleTime = function () {
+        var avg = _totalIdle / _totalServe;
+        if (!isFinite(avg)) {
+          avg = 0;
+        }
+        return avg.toFixed(3);
+      };
 
   return {
-    getId: function () {
-      return _id;
-    },
-    addClient: function (client) {
-      return _queue.push(client);
-    },
-    getTotalServe: function () {
-      return _totalServe;
-    },
-    getQueueLength: function () {
-      return _queue.length;
-    },
+    getId: function () { return _id; },
+    getTotalServe: function () { return _totalServe; },
+    getQueueLength: function () { return _queue.length; },
+    addClient: function (client) { return _queue.push(client); },
+    getAvgIdleTime: _getAvgIdleTime,
+    isIdle: _isIdle,
     serve: function (time) {
       var st, wt;
-      if (!_client && _queue.length > 0) {
-        _client = _queue.shift();
-        _client.startService(time);
-      } else if (_client && _client.getAttendTime() === 0) {
-        _totalServe++;
-        st = _client.getServiceTime();
-        wt = _client.getWaitingTime();
-        console.log('time %d, server %d telah selesai melayani klien %d.', time, _id, _client.getId());
-        _client = null;
-        return [st, wt];
-      } else if (_client) {
+
+      if (_isIdle()) {
+        if (_queue.length > 0) {
+          _totalIdle += _idleTime;
+          _idleTime = 0;
+          _client = _queue.shift();
+          _client.startService(time);
+        } else {
+          _idleTime++;
+        }
+      } else {
         _client.decAttendTime();
+
+        if (_client.getAttendTime() === 0) {
+          _totalServe++;
+          st = _client.getServiceTime();
+          wt = _client.getWaitingTime();
+          console.log('time %d, server %d telah selesai melayani klien %d.', time, _id, _client.getId());
+          _client = null;
+          return [st, wt];
+        }
       }
     },
     getLog: function () {
@@ -41,7 +56,10 @@ var Server = function () {
           id: _client && _client.getId() || '-',
           attend_time: _client && _client.getAttendTime() || '-'
         },
-        queue_length: _queue.length
+        idle_time: _idleTime,
+        total_idle_time: _totalIdle,
+        queue_length: _queue.length,
+        avg_idle_time: _getAvgIdleTime()
       };
     }
   };
